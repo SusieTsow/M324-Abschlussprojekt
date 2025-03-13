9e611ee0f42b4d8bb41a83bf5b66ab5009f48e73
Susie Tsow <susietsow@gmail.com>
2025-03-13 14:04:33 +0100

    Test env variables

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ import todoRoutes from "./routes/todoRoutes.js";
 dotenv.config({ path: "../.env" });
 
 const app = express();
+const TEST_CONNECTION = process.env.TEST_CONNECTION;
+const ANOTHER_NAME = process.env.ANOTHER_NAME;
+const API_URL = process.env.API_URL;
 
 // middleware
 app.use(
@@ -21,7 +24,9 @@ app.use("/api/todos", todoRoutes);
 
 // Health check route
 app.get("/", (req, res) => {
-  res.send("Welcome to the Todo API");
+  res.send(
+    `Text connection is: X${TEST_CONNECTION}X \n Another name is: X${ANOTHER_NAME}X \n api url is: X${API_URL}X`
+  );
 });
 
 // Connect to MongoDB and start the server
