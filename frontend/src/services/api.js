e46bd1b551ae7f0dfe5760d64f198ac39dfd373e
Susie Tsow <susietsow@gmail.com>
2025-03-13 12:44:50 +0100

    Fix CORS again

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.API_URL || "http://localhost:5000/api/todos";
+const API_URL = process.env.API_URL || "http://localhost:5001/api/todos";
 
 export const getTodos = async () => {
   const response = await axios.get(API_URL);
